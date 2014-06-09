7fdb8eaf9dfc2ace36888909484a57c8bbe62728
Craig <craig.crj@gmail.com>
2014-06-09 08:31:03 -0600

    Init.

---
+++ hmm.py
@@ -0,0 +1,12 @@
+# Hidden Markov Model for Part of Speech Tagging
+
+
+
+class POSTagger(object):
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
+
---
+++ llm.py
@@ -0,0 +1,15 @@
+# Language Learning Model
+
+
+
+
+
+
+class LanguageLearner(object):
+    def __init__(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    pass
