479482aa6da97948926d351dc9b46e45724368e1
Craig <craig.crj@gmail.com>
2014-06-09 08:49:52 -0600

    Minimal language learner.

--- llm.py
+++ llm.py
@@ -1,15 +1,39 @@
 # Language Learning Model
 
 
-
-
-
+import random
 
 class LanguageLearner(object):
     def __init__(self):
-        pass
+        self.model = {}
+    
+    def get_const_context(self):
+        return ['']
+    
+    def train(self, tokens):
+        context = self.get_const_context()
+        for token in tokens:
+            model[str(context)] = model.setdefault(str(context), []) + [token]
+            context = (context + [token])[1:]
+    
+    def generate_text(self, number_of_tokens):
+        context = self.get_const_context()
+        for i in xrange(number_of_tokens):
+            word = random.choice(model[str(context)])
+            print(word, end=' ')
+            context = (context+[word])[1:]
+        print()
 
 
 
-if __name__ == "__main__":
-    pass
+if __name__ == '__main__':
+    ll = LanguageLearner()
+    
+    infile = ''
+    with open(infile, 'r') as f:
+        ll.train(f.read().split())
+    
+    ll.generate_text(100)
+        
+    
+    
